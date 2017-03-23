8fc358674a77b021cca75accea09e751113f71b4
Alan Baumgartner <AlanBaumgartner@users.noreply.github.com>
2017-03-23 18:49:35 -0500

    Update instagram_downloader.py

--- instagram_downloader.py
+++ instagram_downloader.py
@@ -16,36 +16,37 @@ async def getJSON(url, session):
         data = await resp.json()
         return data
 
-async def main(username, conns=50, loop=None):
+async def main(usernames, conns=50, loop=None):
     async with aiohttp.ClientSession(loop=loop) as session:
-        sem = asyncio.BoundedSemaphore(conns)
-        url = 'http://instagram.com/' + username + '/media/?max_id='
-        urls = []
-        max_id = ''
-        moreDataToFetch = True
-        while(moreDataToFetch):
-            nextUrl = url + max_id
-            jsonData = await getJSON(nextUrl, session)
-            moreDataToFetch = jsonData['more_available']
-            for item in jsonData['items']:
-                imgUrl = None
-                max_id = item['id']
-                if item['type'] == 'image':
-                    imgUrl = item['images']['standard_resolution']['url']
-                elif item['type'] == 'video':
-                    imgUrl = item['videos']['standard_resolution']['url']
-                urls.append(imgUrl)
+        for username in usernames:
+            sem = asyncio.BoundedSemaphore(conns)
+            url = 'http://instagram.com/' + username + '/media/?max_id='
+            urls = []
+            max_id = ''
+            moreDataToFetch = True
+            while(moreDataToFetch):
+                nextUrl = url + max_id
+                jsonData = await getJSON(nextUrl, session)
+                moreDataToFetch = jsonData['more_available']
+                for item in jsonData['items']:
+                    imgUrl = None
+                    max_id = item['id']
+                    if item['type'] == 'image':
+                        imgUrl = item['images']['standard_resolution']['url']
+                    elif item['type'] == 'video':
+                        imgUrl = item['videos']['standard_resolution']['url']
+                    urls.append(imgUrl)
 
-        tasks = [download_file(username, url, session, sem, loop=loop) for url in urls]
-        await asyncio.gather(*tasks)
+            tasks = [download_file(username, url, session, sem, loop=loop) for url in urls]
+            await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-u", dest='username', help="Instagram username to download photos from",
-                        action="store")
+                        action="store", nargs="+")
     args = parser.parse_args()
-    username = args.username
+    usernames = args.username
 
     loop = asyncio.get_event_loop() 
-    loop.run_until_complete(main(username))
+    loop.run_until_complete(main(usernames))
